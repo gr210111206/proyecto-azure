c55d66822bfa5ceb07e37078f1397efc4acbfc67
gr210111206 <gr210111206@elgrullo.tecmm.edu.mx>
2026-09-17 09:48:45 -0600

    Add Swashbuckle Swagger UI for interactive API documentation at /swagger

--- Program.cs
+++ Program.cs
@@ -2,8 +2,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Enable Swagger UI
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Smart-Support AI Desk API v1");
+    c.RoutePrefix = "swagger";
+});
 
 // Configure the HTTP request pipeline.
 app.UseDefaultFiles(); // Serves index.html by default
